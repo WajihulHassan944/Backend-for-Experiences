91adc5f0cccdde2276f999cdbbcb19df1c3d99c9
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-07-11 18:28:12 +0500

    updates

--- index.js
+++ index.js
@@ -36,6 +36,53 @@ const Experience = mongoose.model('Experience', experienceSchema);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+//Routes
+app.post("/login", (req, res)=> {
+  const { email, password} = req.body
+  User.findOne({ email: email}, (err, user) => {
+      if(user){
+          if(password === user.password ) {
+              res.send({message: "Login Successfull", user: user})
+          } else {
+              res.send({ message: "Password didn't match"})
+          }
+      } else {
+          res.send({message: "User not registered"})
+      }
+  })
+}) 
+
+app.post("/register", (req, res)=> {
+  const { name, email, password} = req.body
+  User.findOne({email: email}, (err, user) => {
+      if(user){
+          res.send({message: "User already registerd"})
+      } else {
+          const user = new User({
+              name,
+              email,
+              password
+          })
+          user.save(err => {
+              if(err) {
+                  res.send(err)
+              } else {
+                  res.send( { message: "Successfully Registered, Please login now." })
+              }
+          })
+      }
+  })
+  
+}) 
+
 // POST /experiences route handler
 app.post('/experiences', async (req, res) => {
   try {
